bf15f844adcbe274e38b85b51a80fae2612640ef
Ogshabzy23101 <ogshabzy23101@gmail.com>
2024-04-20 19:09:25 +0100

    single page cart total amount displayed

--- src/pages/singleProduct.js
+++ src/pages/singleProduct.js
@@ -29,70 +29,72 @@ const image = getElement('.photo-main-image')
 let productId;
 
 window.addEventListener('DOMContentLoaded', async () => {
- const urlsearch = new URLSearchParams(window.location.search)
- const urlId = urlsearch.get('id')
- try {
-  const response = await fetch(`${singleMobileUrl}${urlId}`)
-  if (response.status >= 200 && response.status <= 299) {
-   const products = await response.json()
-   const { battery_capacity, brand,
-    camera_setup, colors, description,
-    display_size,
-    model,
-    price
-    ,
-    processor,
-    ram,
-    release_year,
-    resolution, storage, id
-   } = products
-   productId = id
-   descriptionDOM.innerHTML = `${description}`
-   titleDOM.textContent = `${brand}`
-   modelDOM.textContent = `${model}`
-   yearDOM.textContent = `Year: ${release_year}`
-   priceDOM.textContent = `${formatPrice(price)}`
-   displayDOM.innerHTML = `<span>Display size:</span> ${display_size}`
-   processorDOM.innerHTML = `<span>Processor :</span> ${processor}`
-   batteryDOM.innerHTML = `<span>Battery capacity :</span>${battery_capacity}`
-   ramDOM.innerHTML = `<span>Ram size:</span> ${ram}`
-   storageDOM.innerHTML = `<span>Storage size:</span> ${storage}`
-     resolutionDOM.innerHTML = `<span>resolution:</span> ${resolution}`
-     image.src = `./images/bg/${model.replaceAll(' ', '-')}.png`
+  const urlsearch = new URLSearchParams(window.location.search)
+  const urlId = urlsearch.get('id')
+  try {
+    const response = await fetch(`${singleMobileUrl}${urlId}`)
+    if (response.status >= 200 && response.status <= 299) {
+      const products = await response.json()
+      const { battery_capacity, brand,
+        camera_setup, colors, description,
+        display_size,
+        model,
+        price
+        ,
+        processor,
+        ram,
+        release_year,
+        resolution, storage, id
+      } = products
+      productId = id
+      descriptionDOM.innerHTML = `${description}`
+      titleDOM.textContent = `${brand}`
+      modelDOM.textContent = `${model}`
+      yearDOM.textContent = `Year: ${release_year}`
+      priceDOM.textContent = `${formatPrice(price)}`
+      displayDOM.innerHTML = `<span>Display size:</span> ${display_size}`
+      processorDOM.innerHTML = `<span>Processor :</span> ${processor}`
+      batteryDOM.innerHTML = `<span>Battery capacity :</span>${battery_capacity}`
+      ramDOM.innerHTML = `<span>Ram size:</span> ${ram}`
+      storageDOM.innerHTML = `<span>Storage size:</span> ${storage}`
+      resolutionDOM.innerHTML = `<span>resolution:</span> ${resolution}`
+      image.src = `./images/bg/${model.replaceAll(' ', '-')}.png`
+      CartBtn.setAttribute('data-id', `${productId}`);
 
-   colors.forEach((color) => {
-    const list = document.createElement('li');
-    list.classList.add('product-color');
-    list.textContent = color
-    colorDOM.appendChild(list);
-   });
-
-
-
+      colors.forEach((color) => {
+        const list = document.createElement('li');
+        list.classList.add('product-color');
+        list.textContent = color
+        colorDOM.appendChild(list);
+      });
 
 
-  } else {
-   console.log(response.status, response.statusText);
-   CenterDOM.innerHTML = `
+
+
+
+    } else {
+      console.log(response.status, response.statusText);
+      CenterDOM.innerHTML = `
     <div>
     <h3 class="error">sorry, something went wrong</h3>
     <a href="index.html" class="btn">back home</a>
     </div> 
      `;
+    }
+
+
+
+
+
+
+
+  } catch (error) {
+    console.log(error);
   }
-
-
-
-
-
-
-
- } catch (error) {
-  console.log(error);
- }
 
 })
 
 CartBtn.addEventListener('click', function () {
- addToCart(productId);
+  console.log(productId);
+  addToCart(productId);
 });
